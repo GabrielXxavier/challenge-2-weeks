d54905cc7f983ccb5871eaef5b5835ecdc27f2f2
GabrielXxavier <gabrielsouza252007b@gmail.com>
2025-06-14 03:26:42 -0300

    day 1| implemented class books and chat, the chat for view some options and books for getbooks and set new books

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace challenge_2_weeks
+{
+    public class Chat
+    {
+        Dictionary<string, string> books = new Dictionary<string, string>();
+      
+        public void InitialChat()
+        {
+
+            while (true)
+            {
+                Console.WriteLine(@"
+                    Escolha a função que deseja realizar:
+
+                    Ações Livro:
+                        1 - Ver livros
+                        2 - Cadastrar novo Livro
+                
+                    Ações Autor:
+                        3 - Ver Autores
+                        4 - Cadastrar Novo Autor
+
+                    Ações Categoria:
+                        5 - Ver Categorias
+                        6 - Cadastrar Nova Categoria
+                ");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("Você escolheu ver livros!");
+                        break;
+                    case "2":
+                        Console.WriteLine("Você escolheu cadastrar um novo livro!");
+
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Você escolheu ver livros!");
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Você escolheu ver livros!");
+                        break;
+
+                    case "5":
+                        Console.WriteLine("Você escolheu ver livros!");
+                        break;
+
+                    case "6":
+                        Console.WriteLine("Você escolheu ver livros!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
+            }
+        }
+
+    }
+
+    public class Program
+    {
+        // 👉 O ponto de entrada DO PROGRAMA TEM QUE SER static
+        public static void Main(string[] args)
+        {
+            var chat = new Chat();
+            chat.InitialChat();
+        }
+    }
+}
---
+++ books.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace challenge_2_weeks
+{
+    public class Books
+    {
+        List<Dictionary<string, string>> books = new List<Dictionary<string, string>>();
+        
+        public void setBook()
+        {
+            Dictionary<string, string> book = new Dictionary<string, string>();
+
+            Console.WriteLine("Digite o titulo: ");
+            string title = Console.ReadLine();
+            book.Add("titulo", title);
+
+            Console.WriteLine("Digite a categoria: ");
+            string category = Console.ReadLine();
+            book.Add("categoria", category);
+
+            Console.WriteLine("Digite o valor: ");
+            string value= Console.ReadLine();
+            book.Add("valor", value);
+
+            Console.WriteLine("Digite o autor: ");
+            string author = Console.ReadLine();
+            book.Add("autor", author);
+
+            books.Add(book);
+            
+        }
+
+        public void getBooks()
+        {
+
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
+
+
+  
